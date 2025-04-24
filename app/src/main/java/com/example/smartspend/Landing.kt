49e1b97d1bc95dfae197c502a158e0a93de74c15
AmahleMsomi <amahlemsomi00@gmail.com>
2025-04-24 11:54:25 +0000

    linked pages
    updated code with pages linked

--- app/src/main/java/com/example/smartspend/Landing.kt
+++ app/src/main/java/com/example/smartspend/Landing.kt
@@ -1,11 +1,38 @@
 package com.example.smartspend
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 
 class Landing : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_landing)
+
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.LandingPageLoginbtn)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        //add a register page
+        val LandingPagebtn = findViewById<Button>(R.id.LandingPageLoginbtn)
+        val LandingPageSignUpbtn = findViewById<Button>(R.id.LandingPageSignUpbtn)
+
+
+        LandingPageSignUpbtn.setOnClickListener {
+            val intent = Intent(this, SignUP::class.java)
+            startActivity(intent)
+
+
+            LandingPagebtn.setOnClickListener {
+                val intent = Intent(this, LogIN::class.java)
+                startActivity(intent)
+            }
+        }
     }
 }
--- app/src/main/java/com/example/smartspend/LogIN.kt
+++ app/src/main/java/com/example/smartspend/LogIN.kt
@@ -1,11 +1,30 @@
 package com.example.smartspend
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 
 class LogIN : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_log_in)
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.btnLogIn)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val nextButton = findViewById<Button>(R.id.btnLogIn)
+        nextButton.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
